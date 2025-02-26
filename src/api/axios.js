79cfe51b0f40eef1cd9537ea66f337c25ad16de9
Avinash Kumar <aviluhana@gmail.com>
2025-02-26 23:37:19 +0100

    changed url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: 'http://localhost:8080/api'
+  // baseURL: "http://localhost:8080/api",
+  baseURL: "http://34.226.123.96:8080/api",
 });
